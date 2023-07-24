3c6d73c2f49e5210e89ea1d8161e4da3c53b7c6f
mesri <abde_mes@yahoo.fr>
2023-07-24 14:14:06 +0200

    ctg: Adding ICatalogContext & Impl

---
+++ src/Services/Catalog/Catalog_API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog_API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog_API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
