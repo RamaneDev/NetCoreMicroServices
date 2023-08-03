ec72a5a27067bc8f6468dbb1e930e6fdaf50f511
mesri <abde_mes@yahoo.fr>
2023-08-03 14:03:48 +0200

    Discount_GRPC : Adding Coupon Entity & DicountContext

---
+++ src/Services/Discount/Dicount_GRPC/Data/DiscountContext.cs
@@ -0,0 +1,14 @@
+﻿using Dicount_GRPC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dicount_GRPC.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {        
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
