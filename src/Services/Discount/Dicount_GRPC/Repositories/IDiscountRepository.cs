c55ee437d1787c95802d82a3a1157134682ad5bf
mesri <abde_mes@yahoo.fr>
2023-08-03 14:07:53 +0200

    Discount_GRPC : Adding DiscountRepository

---
+++ src/Services/Discount/Dicount_GRPC/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dicount_GRPC.Entities;
+using System.Threading.Tasks;
+
+namespace Dicount_GRPC.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
