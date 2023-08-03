113ce4677f4ee920507c794db15d6cb94d7a4bae
mesri <abde_mes@yahoo.fr>
2023-08-03 14:39:13 +0200

    Basket_API : Adding DiscountGrpcService after building project

---
+++ src/Services/Basket/Basket_API/GRPC_Services/DiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System;
+using Dicount_GRPC;
+
+namespace Basket_API.GRPC_Services
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
