8ee66e7fe9a64fe3e7527b2e542f3faeacdff86e
mesri <abde_mes@yahoo.fr>
2023-08-03 14:07:02 +0200

    Discount_GRPC : Adding DiscountProfile Mapper

---
+++ src/Services/Discount/Dicount_GRPC/Mapping/DiscountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Dicount_GRPC.Entities;
+
+namespace Dicount_GRPC.Mapping
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
