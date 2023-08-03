f0951817f71f343e15563b155acc752bebae3a4b
mesri <abde_mes@yahoo.fr>
2023-08-03 14:09:18 +0200

    Discount_GRPC : Adding required Services to ServiceProvider & Configuring ConnexionString

--- src/Services/Discount/Dicount_GRPC/Program.cs
+++ src/Services/Discount/Dicount_GRPC/Program.cs
@@ -1,4 +1,11 @@
+using Dicount_GRPC.Data;
+using Dicount_GRPC.Mapping;
+using Dicount_GRPC.Repositories;
 using Dicount_GRPC.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Dicount_GRPC
 {
@@ -13,11 +20,15 @@ namespace Dicount_GRPC
 
             // Add services to the container.
             builder.Services.AddGrpc();
+            builder.Services.AddDbContext<DiscountContext>(o => o.UseNpgsql(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString")));
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+            builder.Services.AddAutoMapper(typeof(DiscountProfile).Assembly);
+            builder.Services.AddGrpc();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
