ef79ffc2b92ee7777aa1eba02df5d947634d25e8
mesri <abde_mes@yahoo.fr>
2023-08-05 00:13:23 +0200

    Ordering_Application: Mapping class

---
+++ src/Services/Ordering/Ordering_Application/Mappings/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Ordering_Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering_Application.Features.Orders.Commands.UpdateOrder;
+using Ordering_Application.Features.Orders.Queries.GetOrdersList;
+using Ordering_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering_Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
