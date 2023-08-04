6488183eba98dda8be1ae2d49b538dffcd90424e
mesri <abde_mes@yahoo.fr>
2023-08-04 16:58:44 +0200

    Ordering: file structure & Adding Interfaces

---
+++ src/Services/Ordering/Ordering_Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using Ordering_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering_Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
---
+++ src/Services/Ordering/Ordering_Domain/Common/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Ordering_Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
