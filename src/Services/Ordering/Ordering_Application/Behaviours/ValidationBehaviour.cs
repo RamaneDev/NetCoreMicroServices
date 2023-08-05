fa5e7d2f16681a9d425c1f2e564fe4055c6b9aba
mesri <abde_mes@yahoo.fr>
2023-08-05 16:59:01 +0200

    Ordering_Application:using our custom ValidationException

--- src/Services/Ordering/Ordering_Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering_Application/Behaviours/ValidationBehaviour.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ValidationException = Ordering_Application.Exceptions.ValidationException;
 
 namespace Ordering_Application.Behaviours
 {
