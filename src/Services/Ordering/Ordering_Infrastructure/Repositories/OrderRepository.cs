ccad043e888df7842320a5c3a12351165301ce68
mesri <abde_mes@yahoo.fr>
2023-08-06 15:39:56 +0200

    Ordering_Infrastruture: Adding BaseRepository class and OrderRepository implementation

---
+++ src/Services/Ordering/Ordering_Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering_Application.Contracts.Persistance;
+using Ordering_Domain.Entities;
+using Ordering_Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering_Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                    .Where(o => o.UserName == userName)
+                                    .ToListAsync();
+            return orderList;
+        }
+    }
+}
