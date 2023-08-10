be44e20c9213c2ac8657fe01466ad75f588e8436
mesri <abde_mes@yahoo.fr>
2023-08-10 14:02:43 +0200

    ocetol: configuring ocetol in Program.cs

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,13 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
 namespace OcelotApiGw
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+
+            builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+            })
+            .ConfigureLogging((hostingContext, loggingbuilder) =>
+            {
+                loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                loggingbuilder.AddConsole();
+                loggingbuilder.AddDebug();
+            });
+
+            builder.Services.AddOcelot()
+                   .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+            var app = builder.Build();           
 
             app.MapGet("/", () => "Hello World!");
+
+            await app.UseOcelot();
 
             app.Run();
         }
